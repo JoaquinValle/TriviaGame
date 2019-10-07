97dc1a83efa8bc649aba511e279ee54c7e74b22e
Joaquin Valle <jv346@cornell.edu>
2019-10-07 10:19:20 -0500

    fixed timer using ids

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,84 @@
 $(document).ready(function(){
 
+    var questions = {
+        questionOne: {
+            question: "Text",
+            answers: {
+                answerA: "A",
+                answerB: "B",
+                answerC: "C",
+                answerD: "D"
+            },
+            correctAnswer: "Correct"
+        },
+        questionTwo: {
+            question: "Text",
+            answers: {
+                answerA: "A",
+                answerB: "B",
+                answerC: "C",
+                answerD: "D"
+            },
+            correctAnswer: "Correct"
+        },
+        questionThree: {
+            question: "Text",
+            answers: {
+                answerA: "A",
+                answerB: "B",
+                answerC: "C",
+                answerD: "D"
+            },
+            correctAnswer: "Correct"
+        },
+        questionFour: {
+            question: "Text",
+            answers: {
+                answerA: "A",
+                answerB: "B",
+                answerC: "C",
+                answerD: "D"
+            },
+            correctAnswer: "Correct"
+        },
+        questionFive: {
+            question: "Text",
+            answers: {
+                answerA: "A",
+                answerB: "B",
+                answerC: "C",
+                answerD: "D"
+            },
+            correctAnswer: "Correct"
+        },
+        questionSix: {
+            question: "Text",
+            answers: {
+                answerA: "A",
+                answerB: "B",
+                answerC: "C",
+                answerD: "D"
+            },
+            correctAnswer: "Correct"
+        },
+    }
+
+    var timeLeft = 30
+    var intervalID
+
+   
+
+
 $("#start").on("click", function() {
     $("#content").text("")
+    intervalID = setInterval(count, 1000)
 
-    var timeText = $("<div>")
-    var timeLeft = 30
+    function count() {
+        timeLeft--
+        timeText.text("Time Remaining: " + timeLeft)
+    }
+
+    var timeText = $("<div id='time'></div> ")
     timeText.text("Time Remaining: " + timeLeft)
     $("#content").append(timeText)
 
