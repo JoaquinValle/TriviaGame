7342395e379cbd9b9f9d980f1d34eceef6f6a06a
Joaquin Valle <jv346@cornell.edu>
2019-10-07 15:20:32 -0500

    added correct and incorrect answer for each click and added question object to add questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,65 +10,64 @@ $(document).ready(function(){
                 answerC: "C",
                 answerD: "D"
             },
-            correctAnswer: "B"
+            correctAnswer: "2"
         },
         questionTwo: {
             count: 2,
-            question: "Text",
+            question: "Test Question Two",
             answers: {
                 answerA: "A",
                 answerB: "B",
                 answerC: "C",
                 answerD: "D"
             },
-            correctAnswer: "C"
+            correctAnswer: "3"
         },
         questionThree: {
             count: 3,
-            question: "Text",
+            question: "Test Question Three",
             answers: {
                 answerA: "A",
                 answerB: "B",
                 answerC: "C",
                 answerD: "D"
             },
-            correctAnswer: "B"
+            correctAnswer: "2"
         },
         questionFour: {
             count: 4,
-            question: "Text",
+            question: "Test Question Four",
             answers: {
                 answerA: "A",
                 answerB: "B",
                 answerC: "C",
                 answerD: "D"
             },
-            correctAnswer: "A"
+            correctAnswer: "1"
         },
         questionFive: {
             count: 5,
-            question: "Text",
+            question: "Test Question Five",
             answers: {
                 answerA: "A",
                 answerB: "B",
                 answerC: "C",
                 answerD: "D"
             },
-            correctAnswer: "C"
+            correctAnswer: "3"
         },
         questionSix: {
             count: 6,
-            question: "Text",
+            question: "Test Question Six",
             answers: {
                 answerA: "A",
                 answerB: "B",
                 answerC: "C",
                 answerD: "D"
             },
-            correctAnswer: "D"
+            correctAnswer: "4"
         },
     }
-
     var timeLeft = 10
     var intervalID
     var questionCount = 1
@@ -80,7 +79,7 @@ $("#start").on("click", function() {
     function count() {
         timeLeft--
         timeText.text("Time Remaining: " + timeLeft)
-        console.log(timeLeft)
+        //console.log(timeLeft)
         if (timeLeft === 0) {
             clearInterval(intervalID)
             questionText.text("Nope! Wrong Answer")
@@ -93,11 +92,15 @@ $("#start").on("click", function() {
         function answerClick(clicked) {
             $("#" + clicked).on("click", function() {
                 clearInterval(intervalID)
-                questionText.text("Answer might be right")
+                if ($(this).attr("id") === questions.questionOne.correctAnswer) {
+                    questionText.text("Correct Answer")
+                }
+                else {
+                questionText.text("Incorrect Answer")
+                }
+                
             })
         }
-        
-
     }
 
     var timeText = $("<div id='time'></div> ")
@@ -112,7 +115,13 @@ $("#start").on("click", function() {
     var options = $("<div id='options'></div>")
     $("#content").append(options)
 
-    var possibleAnswers = ["answer 1", "answer 2", "answer 3", "answer 4"]
+    var possibleAnswers = []
+
+    if (questionCount === 1) {
+    possibleAnswers = [questions.questionOne.answers.answerA,questions.questionOne.answers.answerB,
+                       questions.questionOne.answers.answerC,questions.questionOne.answers.answerD]
+    }
+
 
 for (i = 0; i < possibleAnswers.length; i++) {
     var optionText = $("<div id='" + (i+1) + "'></div>")
@@ -120,9 +129,5 @@ for (i = 0; i < possibleAnswers.length; i++) {
     optionText.text(option)
     $("#options").append(optionText)
 }
-
 })
-
-
-
 })
